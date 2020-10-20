5950faac7962cca5f7b4cc892952c80317d833e6
Arthurcn96 <Arthurcn96@gmail.com>
2020-10-20 08:48:09 -0300

    Creating route files

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,15 @@
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi'
+
 
 import './styles/global.css'
-import './styles/pages/landing.css'
 
-import logoImg from './images/Logo.svg'
+import Routes from './routes'
+import { Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div id="page-landing">
-
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy"/>
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças</p>
-        </main>
-
-        <div className="location">
-          <strong>Pinheiros</strong>
-          <span>Espirito Santo</span>
-        </div>
-
-        <a href="void()" className="enter-app">
-        <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
-        </a>
-
-      </div>
-
-    </div>
+    <Routes />
   );
 }
 
