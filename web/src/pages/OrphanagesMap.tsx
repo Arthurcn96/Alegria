74606b6c5a7b9a48dd96fa9c108955475d8b3245
Arthurcn96 <Arthurcn96@gmail.com>
2020-10-22 22:52:22 -0300

    Updating tsx files

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {FiPlus} from 'react-icons/fi'
+
+import mapMarkerImg from '../images/Local.svg';
+
+import '../styles/pages/OrphanagesMap.css';
 
 function OrphanagesMap(){
     return(
-        <h1>Hello World</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+
+                </header>
+
+                <footer>
+                    <strong>Pinheiros</strong>
+                    <span>Espírito Santo</span>
+                </footer>
+
+            </aside>
+
+            <div></div>
+
+        <Link to="" className="create-orphanage">
+            <FiPlus size={32} color="#FFF"/>
+        </Link>
+        </div>
     );
 }
 
